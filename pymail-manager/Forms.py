b28b518118c0c07f9fa0138d6307e2e006028976
pankajthekush <pankajthekush@gmail.com>
2018-04-08 20:23:32 +0530

    Added the Email Send Window

--- pymail-manager/Forms.py
+++ pymail-manager/Forms.py
@@ -1,17 +1,21 @@
 import tkinter as tk
-
+from tkinter import *
 
 
 class Forms:
 
     def __init__(self,loginAPiClass):
         self.root = tk.Tk()
+        #self.root.resizable(False, False)
         self.lbl_username = tk.Label(text="User Name")
         self.lbl_password = tk.Label(text="Password")
         self.txt_Username = tk.Entry()
         self.txt_Password = tk.Entry(show="*")
         self.loginAPiClass  = loginAPiClass
+        self.sender_email_id = None
         self.status = False
+
+
     def LoginForm(self):
 
         self._login_status = False
@@ -42,6 +46,7 @@ class Forms:
             self.msgbox("Error While Logging In","Error")
         else:
              self.status = True
+             self.sender_email_id = uname + "@gmail.com"
              self.root.destroy()
 
     def msgbox(self,MessageString,titleString):
@@ -61,6 +66,71 @@ class Forms:
         # msgroot.mainloop()
 
     def SendEmail(self):
-        self.root.title("Login Form")
-        self.root.geometry("300x100")
+
+
+        self.root.title("Send And Email")
+        self.root.geometry("568x500")
+        for rw in range(10):
+            self.root.rowconfigure(rw,weight=1)
+
+        for cl in range(5):
+            self.root.columnconfigure(cl,weight=1)
+
+
+        frame_email_info = tk.Frame(bg="red")
+        frame_email_info.grid(row = 0, column = 0, rowspan = 1, columnspan = 3, sticky = W+E+N+S)
+
+
+        blank_Cell_receiver = tk.Label(frame_email_info,text="self.root")
+        #NOW PUT THE SENDER EMAIL AND RECEIVER EMAIL FIELD IN THIS FRAME
+
+        #ADDING LABLES and textboxes
+
+        #sender
+        blank_Cell_sender = tk.Label(frame_email_info,text="        ") #TO KEEP THE SPACING BETWEEN THE COLUMNS , BECAUSE THE DEFAULT CELL SIZE IS 0
+
+        lbl_senderEmail = tk.Label(frame_email_info,text="Sender Email")
+        lbl_senderEmail.grid(row=0,column=0)
+
+
+        txt_sender = tk.Entry(frame_email_info)
+        txt_sender.grid(row=0,column=3)
+        blank_Cell_sender.grid(row=0,column=2)
+
+        #RECEIVER
+
+        blank_Cell_Receiver = tk.Label(frame_email_info,text="        ") #TO KEEP THE SPACING BETWEEN THE COLUMNS , BECAUSE THE DEFAULT CELL SIZE IS 0
+
+        lbl_receiveremail = tk.Label(frame_email_info,text="Sender Email")
+        lbl_receiveremail.grid(row=1,column=0)
+
+
+        txt_receiver = tk.Entry(frame_email_info)
+        txt_receiver.grid(row=1,column=3)
+        blank_Cell_Receiver.grid(row=1,column=2)
+
+        #NOW THE EMAIL BODY
+
+        Frame_TextBody = tk.Frame(bg="red")
+        Frame_TextBody.grid(row = 1, column = 0, rowspan = 3, columnspan = 4, sticky = W+E+N+S)
+
+
+
+        txt_Email_Body = tk.Text(Frame_TextBody,height=30, width=80)
+        #SCROLLBAR
+        self.root.rowconfigure(1, weight=3)
+
+        scroll = Scrollbar(self.root, command=txt_Email_Body.yview)
+        txt_Email_Body.configure(yscrollcommand=scroll.set)
+        txt_Email_Body.grid(row=0,column=0)
+        scroll.grid(row=1,column=10,sticky='NS')
+
+
+        #SEND EMAIL Button
+
+        btn_send_Email = tk.Button(self.root,text="Send Email")
+        btn_send_Email.grid(row=4,column=0)
+
+
+
         self.root.mainloop()
--- pymail-manager/SendEmail2.py
+++ pymail-manager/SendEmail2.py
@@ -2,13 +2,19 @@ from Forms import Forms
 from EmailApi import LoginToGmail
 lg = LoginToGmail()
 
-frm = Forms(lg)
-frm.LoginForm()
+#frm = Forms(lg)
+# frm.LoginForm()
 
-if frm.status:
-    print("We will Send Email Now")
-    frm = None
-    frm = Forms(lg)
-    frm.SendEmail()
-else:
-    print("Email Send Module Not Loaded")
+frm = Forms(lg)
+frm.SendEmail()
+
+# if frm.status:
+#     print("We will Send Email Now")
+#     frm = None
+#     frm = Forms(lg)
+#     frm.SendEmail()
+# else:
+#     print("Email Send Module Not Loaded")
+#     frm = None
+#     frm = Forms(lg)
+#     frm.SendEmail()
